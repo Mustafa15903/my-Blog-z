85949abb8fd1358b1b3208a2426ccc0ef4b0e7bf
MUSTAFA <mustafa7io2002@gmail.com>
2025-07-18 06:52:49 +0300

    edit home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,18 @@ function Home() {
   return (
     <div className="page">
       <h1>Welcome to My Blog 👨‍💻 </h1>
-      <p>This is the homepage  latest posts.</p>
+      <div className="intro" style={{ maxWidth: '600px', margin: '0 auto', textAlign: 'center' , fontStyle: 'italic'}}>
+      <p>Hello, my name is Mustafa. My nickname is Zashh, 
+        a name I gave myself inspired by Zeus, the mythical Greek god, because at one point in my life I was fascinated by mythology.
+      </p>
+      <p>I’m a creative writer, art lover, and web developer, aspiring to become a successful programmer.
+      </p>
+      <p>I always move from one experience to another — either I win or I learn a lesson.
+      </p>
+      <p>"One day, we will win life's bet, for our struggle for our dreams was never in vain."
+      </p>
       <Link to="/blog" className="button">Visit Blog</Link>
+    </div>
     </div>
   );
 }
